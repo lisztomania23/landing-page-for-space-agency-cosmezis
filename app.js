ed0ce838c2caf05bb83b5816632e5b283c91fc42
lisztomania23 <97365025+lisztomania23@users.noreply.github.com>
2023-10-25 20:32:28 +0530

    transitions, button, and some effects

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+            entry.target.classList.remove('no-webkit-fill');
+            setTimeout(() => {
+                entry.target.classList.add('no-webkit-fill');
+            }, 1000);
         } else {
             entry.target.classList.remove('show');
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden,.hidden-hero-text');
+const hiddenElements = document.querySelectorAll('.hidden, .hidden-hero-text, .hiddenimg, .hiddenhero');
 hiddenElements.forEach((el) => observer.observe(el));
